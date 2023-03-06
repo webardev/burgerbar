087397638617a5a74ca4b2366347a078d90126e1
Rafay <125589418+webardev@users.noreply.github.com>
2023-03-06 20:41:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let loader = document.getElementById("preloader")
+
+
+
+// setTimeout(() => {
+//   loader.style.opacity = "0";
+//   loader.style.display = "none";
+  
+// }, 2928)
+
+
+window.addEventListener("load", function(){
+  loader.style.display = "none"
+})
